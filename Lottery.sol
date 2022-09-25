f965e5fe40415d27f3c9967f45295ab151c2fbc9
Rohit Kumar <67726628+rohitverse@users.noreply.github.com>
2022-09-25 17:47:15 +0530

    Add files via upload

---
+++ Lottery.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Lottery{
+    address public manager;
+    address payable[] public participants;
+
+    constructor(){
+        manager=msg.sender;
+    }
+
+    receive() external payable{
+        require(msg.value==1 ether);
+        participants.push(payable (msg.sender));
+    }
+
+    function getBalance() public view returns (uint){
+        require(manager==msg.sender);
+        return address(this).balance;
+    }
+
+    function random() public view returns(uint){
+        require(manager==msg.sender);
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
+    }
+
+    function selectWinner() public
+     //view returns(address)
+    {
+        require(msg.sender==manager);
+        require(participants.length>=3);
+
+        uint r=random();
+        address payable winner;
+        uint index=r%participants.length;
+        winner=participants[index];
+       // return winner;
+        winner.transfer(getBalance());
+        participants=new address payable[](0); 
+    }
+}
